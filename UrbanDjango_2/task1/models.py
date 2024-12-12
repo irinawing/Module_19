098cfbc974e677b64f63e17b649db26606a6d4b9
IrinaWing <irina.wing@mail.ru>
2024-12-13 01:20:29 +0200

    Module_19_2: QuerySet запросы в базу данных

--- UrbanDjango_2/task1/models.py
+++ UrbanDjango_2/task1/models.py
@@ -15,7 +15,7 @@ class Game(models.Model):
     size = models.DecimalField(max_digits=10, decimal_places=2)  # Размер файлов игры
     description = models.TextField()  # Описание игры
     age_limited = models.BooleanField(default=False)  # Ограничение по возрасту
-    buyers = models.ManyToManyField(Buyer, related_name='games')  # Покупатели
+    buyers = models.ManyToManyField(Buyer, related_name='games', default=None)  # Покупатели
 
     def __str__(self):
         return self.title
