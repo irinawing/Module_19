c503fb6380ed4e1d140424a2b58d764eb6f8e04a
IrinaWing <irina.wing@mail.ru>
2024-12-14 23:15:54 +0200

    Module_19_4: Сайт администрирования

--- UrbanDjango_2/task1/admin.py
+++ UrbanDjango_2/task1/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Buyer, Game
 
-# Register your models here.
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size')
+    search_fields = ('title',)
+    list_filter = ('size', 'cost')
+    fields = [('title', 'size', 'cost'), 'description', 'age_limited', 'buyer']
+    list_per_page = 20
+
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'balance', 'age')
+    search_fields = ('name',)
+    list_filter = ('balance', 'age')
+    list_per_page = 30
